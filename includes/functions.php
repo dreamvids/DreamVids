0314817d8941ae2d283129ba4acc36cc0830a61d
Quadrifoglio <quadrifoglio.clement@gmail.com>
2013-12-13 20:46:38 +0100

    Created the basics of the configuration manager

--- includes/functions.php
+++ includes/functions.php
@@ -1,3 +1,13 @@
 <?php
 
+function initDataBaseConfig($dbHost, $dbUsername, $dbPassword) {
+    $GLOBALS['config'] = array(
+        'mysql' => array(
+            'host' => $dbHost,
+            'username' => $dbUsername,
+            'password' => $dbPassword,
+        )
+    );
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+
+include('includes/functions.php');
+require_once('classes/Config.php');
+
+session_start();
+//TODO: Update mysql connection settings
+initDataBaseConfig('127.0.0.1', 'root', '');
+
 echo '<h1 style="text-align:center">Welcome to DreamVids.Fr !</h1>';
+
+// This is how to get the mysql host from config
+echo 'Mysql host: '.Config::get('mysql/host');
+
 ?>
