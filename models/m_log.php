cbd2dd8b93e80945f65483e7fb1a3e72c9942f90
PHPeter <phpeter@phpeter.fr>
2013-12-14 16:09:03 +0100

    Begin login and adding templates top and bottom

--- models/m_log.php
+++ models/m_log.php
@@ -1,3 +1,6 @@
 <?php
-
+class Log
+{
+	
+}
 ?>
---
+++ views/_top.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>DreamVids</title>
+		<meta charset="utf-8" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link href="css/style.css" rel="stylesheet" />
+	</head>
+	
+	<body>
