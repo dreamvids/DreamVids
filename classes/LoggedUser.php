3fa3b80f8c894c4400f35b8f9977f408282225c6
Quadrifoglio <quadrifoglio.clement@gmail.com>
2013-12-14 14:33:37 +0100

    Created the basics of the User class (getters/setters)

--- classes/LoggedUser.php
+++ classes/LoggedUser.php
@@ -7,7 +7,7 @@ class LoggedUser extends User {
     private $ip;
     
     public function __construct($name, $ip) {
-        parent::__construct($name);
+        parent::__construct();
         $this->ip = $ip;
     }
     
--- classes/User.php
+++ classes/User.php
@@ -2,14 +2,69 @@
 
 class User {
     
+    private $id;
     private $name;
-    
-    public function __construct($name) {
-        $this->name = $name;
+    private $mail;
+    private $avatar;
+    private $subscribers;
+    private $rank;
+
+    public function __construct() {
+        loadDataFromDatabase();
     }
+
+    public function loadDataFromDatabase() {
+    	//TODO: Load fields values from DB
+    }
+
+    public function saveDataToDatabase() {
+    	//TODO: Save fields value to DB
+    }
+
+    public function setEmailAddress($newMail) {
+        $this->mail = $newMail;
+        saveDataToDatabase();
+    }
+
+    public function setAvatarPath($newAvatar) {
+        $this->avatar = $newAvatar;
+        saveDataToDatabase();
+    }
+
+    public function setSubscribers($newSubscribers) {
+        $this->subscribers = $newSubscribers;
+        saveDataToDatabase();
+    }
+
+    public function setRank($newRank) {
+        $this->rank = $newRank;
+        saveDataToDatabase();
+    }
+
+    //
     
+	public function getId() {
+        return $this->id;
+    }
+
     public function getName() {
         return $this->name;
+    }
+
+    public function getEmailAddress() {
+        return $this->mail;
+    }
+
+    public function getAvatarPath() {
+        return $this->avatar;
+    }
+
+    public function getSubscribers() {
+        return $this->subscribers;
+    }
+
+    public function getRank() {
+        return $this->rank;
     }
 }
 
