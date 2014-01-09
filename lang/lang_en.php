5d920548626fe2aba00955774255bd9ca19f35e6
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2014-01-09 08:01:19 +0100

    Create lang_en.php
    J'ai créer la langue anglaise pour vous faire gagner du temps ;-)
    À continuer...

---
+++ lang/lang_en.php
@@ -0,0 +1,47 @@
+<?php
+$lang = array(
+'error_reg_username_jam' => 'All usernames proposed are already used.',
+
+'error_reg_userlen' => 'The username shouldn't exceed 40 characters',
+
+'error_reg_username' => 'This username is already used.',
+
+'error_reg_email' => 'This E-Mail is already used.',
+
+'error_reg_empty' => 'All fields must be filled',
+
+'error_reg_email_false' => 'You must enter a valid E-Mail address.',
+
+'error_reg_equality_pass' => 'Your password and confirmation password do not match.',
+
+'error' => 'Error',
+
+'reg_ok' => 'Successful registration! <a href="login">Sign in</a> now !',
+
+'email_address' => 'E-Mail Address',
+
+'username' => 'Username',
+
+'password' => 'Password',
+
+'password_confirm' => 'Confirm password',
+
+'register' => 'Register !',
+
+'error_log_username' => 'Cet utilisateur n\'existe pas.',
+
+'error_log_pass' => 'Mot de passe incorrect.',
+
+'login' => 'Connexion',
+
+'log_ok' => 'Connexion réussie !',
+
+'remember' => 'Se souvenir de moi',
+
+'profile_ok' => 'Toutes les modifications ont été enregistrées !',
+
+'profile_update' => 'Mettre à jours mes informations',
+
+'avatar' => 'Avatar'
+);
+?>
