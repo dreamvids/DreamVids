36daeb658560e2de85ebf6ac4efb99a00dcb792c
Your Name Here <quadrifoglio.clement@gmail.com>
2014-01-20 17:56:50 +0100

    Prevent non logged users to access the subscriptions page

--- pages/vidslist.php
+++ pages/vidslist.php
@@ -8,6 +8,11 @@ switch (@$_GET['mode'])
 	break;
 	
 	case 'subscriptions':
+		if(!isset($session)) {
+			header('Location: ./?page=log');
+			exit();
+		}
+
 		$vids = $vidslist->getSubscriptionsVideos(100);
 		$subs = $vidslist->getSubscriptions();
 		$title = $lang['subscriptions'];
