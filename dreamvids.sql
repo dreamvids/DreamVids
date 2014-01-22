a006821dc3cc460d8239d5aa34ff86a3ae4e6724
Vetiore <phpeter@phpeter.fr>
2014-01-22 19:15:51 +0100

    Update dreamvids.sql

--- dreamvids.sql
+++ dreamvids.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 3.4.11.1deb2
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 03 Janvier 2014 à 20:38
--- Version du serveur: 5.6.12-log
--- Version de PHP: 5.4.12
+-- Généré le: Mer 22 Janvier 2014 à 18:09
+-- Version du serveur: 5.5.33
+-- Version de PHP: 5.4.23-1~dotdeb.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données: `dreamvids`
 --
-CREATE DATABASE IF NOT EXISTS `dreamvids` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `dreamvids`;
 
 -- --------------------------------------------------------
 
@@ -32,6 +30,14 @@ CREATE TABLE IF NOT EXISTS `config` (
   `key` varchar(255) NOT NULL,
   `value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `config`
+--
+
+INSERT INTO `config` (`key`, `value`) VALUES
+('rank_mbr', '1'),
+('rank_adm', '9');
 
 -- --------------------------------------------------------
 
@@ -46,13 +52,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pass` varchar(40) NOT NULL,
   `avatar` varchar(255) NOT NULL,
   `subscribers` int(11) NOT NULL,
+  `subscriptions` text NOT NULL,
   `reg_timestamp` bigint(20) NOT NULL,
   `reg_ip` varchar(15) NOT NULL,
   `actual_ip` varchar(15) NOT NULL,
   `rank` int(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 -- --------------------------------------------------------
 
@@ -64,8 +70,7 @@ CREATE TABLE IF NOT EXISTS `users_sessions` (
   `user_id` int(11) NOT NULL,
   `session_id` varchar(32) NOT NULL,
   `expiration` bigint(20) NOT NULL,
-  `remember` tinyint(1) NOT NULL,
-  UNIQUE KEY `session_id` (`session_id`)
+  `remember` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -75,16 +80,21 @@ CREATE TABLE IF NOT EXISTS `users_sessions` (
 --
 
 CREATE TABLE IF NOT EXISTS `videos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(6) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
+  `tags` text NOT NULL,
+  `tumbnail` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `views` bigint(20) NOT NULL,
   `likes` int(11) NOT NULL,
   `dislikes` int(11) NOT NULL,
+  `timestamp` bigint(20) NOT NULL,
+  `visibility` int(11) NOT NULL,
+  `flagged` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
