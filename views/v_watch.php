ef3effdadeadb1727144ddf0b6c0db00f0ed6aae
PHPeter <phpeter@phpeter.fr>
2014-01-27 00:27:48 +0100

    Correct twitter button bug

--- views/v_watch.php
+++ views/v_watch.php
@@ -95,7 +95,7 @@
 		<?php
 	}
 	?>
-		<a href="https://twitter.com/share" class="twitter-share-button" data-text="''<?php echo (strlen($title) > 50) ? substr($title, 0, 50).'...' : $title; ?>'' sur @DreamVids_ ! Check this out ! http://dreamvids.fr/page=watch&vid=<?php echo htmlspecialchars($_GET['vid']); ?>" data-lang="fr">Tweeter</a>
+		<a href="https://twitter.com/share" class="twitter-share-button" data-text="''<?php echo (strlen($title) > 50) ? substr($title, 0, 50).'...' : $title; ?>'' sur @DreamVids_ ! Check this out !" data-lang="fr">Tweeter</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 		<div id="fb-root"></div>
 <script>(function(d, s, id) {
