0cdd73109de61559f3401115f37ffead8294497b
PHPeter <phpeter@phpeter.fr>
2014-01-26 23:00:01 +0100

    ToDo task: add social sharing buttons on watch page

--- views/v_watch.php
+++ views/v_watch.php
@@ -95,6 +95,31 @@
 		<?php
 	}
 	?>
+		<a href="https://twitter.com/share" class="twitter-share-button" data-text="''<?php echo (strlen($title) > 50) ? substr($title, 0, 50).'...' : $title; ?>'' sur @DreamVids_ ! Check this out ! http://dreamvids.fr/page=watch&vid=<?php echo htmlspecialchars($_GET['vid']); ?>" data-lang="fr">Tweeter</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-share-button" data-href="http://dremavids.fr/page=watch&vid=<?php echo htmlspecialchars($_GET['vid']); ?>" data-type="button_count"></div><br />
+<!-- Placez cette balise où vous souhaitez faire apparaître le gadget Bouton +1. -->
+<div class="g-plusone" data-size="small" data-href="http://dreamvids.fr/page=watch&vid=<?php echo htmlspecialchars($_GET['vid']); ?>"></div>
+
+<!-- Placez cette ballise après la dernière balise Bouton +1. -->
+<script type="text/javascript">
+  window.___gcfg = {lang: 'fr'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+	<br /><br />
 	<div class="panel panel-primary" style="width: 56%;">
 		<div class="panel-heading">
 			<?php echo $lang['desc']; ?>
