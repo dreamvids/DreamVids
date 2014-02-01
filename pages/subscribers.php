834a7d9d933faa20d30f79917001ca15019afa17
PHPeter <phpeter@phpeter.fr>
2014-02-01 22:40:42 +0100

    bugfix

--- pages/subscribers.php
+++ pages/subscribers.php
@@ -1,14 +1,16 @@
 <?php
 
-if(!isset($session) | !isset($_GET['uid'])) {
-	header('Location: ./?page=log');
+if(!isset($session) || !isset($_GET['uid']) ) {
+	//header('Location: ./?page=log');
+	echo 'C\'est celle de la session qui merde !';
 	exit();
 }
 
 $user = new User(htmlentities(mysql_real_escape_string($_GET['uid'])));
 
 if($user->getId() <= 0) {
-	header('Location: ./');
+	//header('Location: ./');
+	echo 'C\'est celle du paramètre d\'URL qui merde !';
 	exit();
 }
 
