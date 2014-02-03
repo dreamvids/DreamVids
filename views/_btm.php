1764cced92a0d8814f97d9ace342c4237020afc9
VincentBanana <vincent.savoy@bluewin.ch>
2014-02-03 19:44:51 +0100

    Update _btm.php

--- views/_btm.php
+++ views/_btm.php
@@ -2,17 +2,17 @@
 		<div class="container separator" style="width: 80%;"></div>
 		<h4 style="color:grey;margin-top:40px;margin-bottom:40px;text-align:center;">
 			Librement réalisé par:
-			<a href="http://twitter.com/BokoratMC">Bokorat</a>, 
-			<a href="http://twitter.com/JouetR">BrezhDev</a>, 
-			<a href="http://twitter.com/Sebraecha">Charlie</a>, 
-			<a href="http://twitter.com/DarkWos1">Dark Wos</a>, 
-			<a href="http://twitter.com/jeremy__fr">Jeremy</a>, 
-			<a href="http://twitter.com/Dark_Tagnan">Jonathan</a>, 
-			<a href="http://twitter.com/oliviermis">Olivier</a>, 
-			<a href="http://twitter.com/p_cauty">PHPeter</a>, 
-			<a href="http://twitter.com/_quadrifoglio">Quadrifoglio</a>, 
-			<a href="http://twitter.com/VincentBanana">Vincent Banana</a> 
-			et <a href="http://skype.com/">La bonne humeur</a> !
+			<a href="http://twitter.com/BokoratMC" target="_blank">Bokorat</a>, 
+			<a href="http://twitter.com/JouetR" target="_blank">BrezhDev</a>, 
+			<a href="http://twitter.com/Sebraecha" target="_blank">Charlie</a>, 
+			<a href="http://twitter.com/DarkWos1" target="_blank">DarkWos</a>, 
+			<a href="http://twitter.com/jeremy__fr" target="_blank">Jeremy</a>, 
+			<a href="http://twitter.com/Dark_Tagnan" target="_blank">Jonathan</a>, 
+			<a href="http://twitter.com/oliviermis" target="_blank">Olivier</a>, 
+			<a href="http://twitter.com/p_cauty" target="_blank">PHPeter</a>, 
+			<a href="http://twitter.com/_quadrifoglio" target="_blank">Quadrifoglio</a>, 
+			<a href="http://twitter.com/VincentBanana" target="_blank">VincentBanana</a> 
+			et <a href="http://skype.com/" target="_blank">La bonne humeur</a> !
 		</h4>
 	</div>
 	
