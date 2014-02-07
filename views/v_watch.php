d9595fc96d11bb77b09640eba812d82752a29667
Quadrifoglio <quadrifoglio.clement@gmail.com>
2014-02-07 18:15:15 +0100

    Trying to fix dislikes, the return n°3

--- views/v_watch.php
+++ views/v_watch.php
@@ -100,7 +100,7 @@
 			<img src="img/videos/positive.png" <?php if($log){ ?>onclick="like('<?php echo secure($_GET['vid']); ?>')"<?php } ?> width="32" style="cursor:pointer" alt="Like" /> <span <?php echo $isLiked; ?> id="like-<?php echo secure($_GET['vid']); ?>"><?php echo $likes; ?></span>&nbsp;&nbsp;&nbsp;&nbsp;<img src="img/videos/negative.png" <?php if($log){ ?>onclick="dislike('<?php echo secure($_GET['vid']); ?>')"<?php } ?> width="32" style="cursor:pointer" alt="Dislike" /> <span <?php echo $isDisliked; ?> id="dislike-<?php echo secure($_GET['vid']); ?>"><?php echo $dislikes; ?></span>
 			<?php
 		}
-		<?php
+		?>
 		<b style="margin-left:50px"><?php echo $video->getViews() ?> vues</b></div>
 		<br /><br />
 
