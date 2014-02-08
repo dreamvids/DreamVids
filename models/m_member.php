799fbcd6de92ecf633147b4a9d34f33145e00828
Quadrifoglio <quadrifoglio.clement@gmail.com>
2014-02-08 17:18:02 +0100

    Showing all videos in channel view

--- models/m_member.php
+++ models/m_member.php
@@ -5,7 +5,8 @@ class Member {
 	public static function getVideosFromUsers($userId, $LIMIT="4") {
 		$vids = array();
 		$db = new BDD();
-		$res = $db->select('*', 'videos', 'WHERE user_id='.$userId.' ORDER BY timestamp DESC LIMIT 0,'.$LIMIT);
+		//$res = $db->select('*', 'videos', 'WHERE user_id='.$userId.' ORDER BY timestamp DESC LIMIT 0,'.$LIMIT);
+		$res = $db->select('*', 'videos', 'WHERE user_id='.$userId.' ORDER BY timestamp DESC');
 
 		$i = 0;
 		while($row = $db->fetch_array($res)) {
