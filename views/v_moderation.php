2ba66d844f73582fc067f87466cb6e4e45b20c61
Bestt-Alex <mrsaooty@gmail.com>
2014-02-17 01:30:27 +0100

    Update v_moderation.php

--- views/v_moderation.php
+++ views/v_moderation.php
@@ -20,8 +20,8 @@
 			foreach ($flaggedVids as $vid) {
 				?>
 
-				<a href="/watch-<?php echo $vid->getId(); ?>" class="list-group-item">
-					<h4 class="list-group-item-heading"><?php echo $vid->getTitle().' - By '.User::getNameById($vid->getUserId()); ?></h4>
+				<a href="/&<?php echo $vid->getId(); ?>" class="list-group-item">
+					<h4 class="list-group-item-heading"><?php echo $vid->getTitle(); echo ' - ' . $lang['by'] . ''; echo User::getNameById($vid->getUserId()); ?></h4>
 					<p class="list-group-item-text"><?php echo bbcode(nl2br(secure($vid->getDescription()) ) ); ?></p>
 				</a>
 
