4ae2819a477338e86ee3b0d4ff892d138f7aebad
ThePico20 <pico54136@gmail.com>
2014-04-13 20:53:50 +0200

    Ajout d'une colonne pourla date de publication d'un bug

--- database.sql
+++ database.sql
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `bugs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL,
   `user_id` int(11) NOT NULL,
   `description` text NOT NULL,
   `url` varchar(255) NOT NULL,
