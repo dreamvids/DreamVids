c6c31b96a42a9a69c933b66f8bb16e6c87e26c67
PHPeter <phpeter@phpeter.fr>
2014-06-25 18:28:41 +0200

    BugFix manager

--- pages/manager.php
+++ pages/manager.php
@@ -5,14 +5,14 @@ if(!isset($session)) {
 	exit();
 }
 
-if(empty($vids)) {
-	$err = $lang['error_no_videos_uploaded'];	
-}
-
 if(isset($_GET['delVid'])) {
 	$vidToDelId = htmlentities(mysql_real_escape_string($_GET['delVid']));
 	Manager::deleteVideo($vidToDelId, $session->getId());
 }
 
 $vids = Manager::getVideosFromUser($session->getId());
+
+if(empty($vids)) {
+	$err = $lang['error_no_videos_uploaded'];	
+}
 ?>
