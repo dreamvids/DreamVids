cb526778b0711c7124c1c70bcd4a2f6afebb1e1d
ThePico20 <pico54136@gmail.com>
2014-07-29 16:09:43 +0200

    Front-end webchat

---
+++ chat/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+	</head>
+	<body>
+		<div id="chat">
+			<div class="message">
+				<a href="http://dreamvids.fr/@User"><img src="http://placehold.it/30x30" class="avatar" /><div class="pseudo">User</div></a><div class="text">From user</div>
+			</div>
+			<div class="message me">
+				<a href="http://dreamvids.fr/@User"><img src="http://placehold.it/30x30" class="avatar" /><div class="pseudo">Me</div></a><div class="text">From sender</div>
+			</div>
+		</div>
+		<div id="send">
+			<form>
+				<input type="text" placeholder="Entrez votre message">
+				<input type="submit" class="btn btn-sm btn-default" value="Envoyer">
+			</form>
+		</div>
+	</body>
+</html>
