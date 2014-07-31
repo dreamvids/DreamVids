6b3f0fed46d684782f3ba7dcbd3b7098519a7c5f
LowiSky <lowisky@gmail.com>
2014-07-31 12:21:46 +0200

    Front - Suspended video page

--- app/views/video/suspended.php
+++ app/views/video/suspended.php
@@ -1,1 +1,15 @@
-<h1>La vidéo '<?php echo $video->title; ?>' à été suspendue.</h1>
+<div id="suspended-icn">
+	<div id="cross">
+		<div class="cross-bar" id="cross-bar-1"></div>
+		<div class="cross-bar" id="cross-bar-2"></div>
+	</div>
+</div>
+<div id="suspended-text">
+	<h2>La vidéo <span class="video-name"><?php echo $video->title; ?></span> à été suspendue.</h2>
+	<p>Retour à la chaîne :</p>
+	<div class="channel-access">
+		<img src="<?php echo IMG.'avatar_user.png'; ?>" alt="Avatar de la chaîne">
+		<p>Nom de la chaîne</p>
+		<a href=""><div class="channel-access-btn"><img src="<?php echo IMG.'arrow_right.png'; ?>" alt="Allez sur la chaîne"></div></a>
+	</div>
+</div>
