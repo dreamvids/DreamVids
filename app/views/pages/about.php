81fde70ff4c9dafbe2177cf474b8a4dc4cc3ae52
PHPeter <phpeter@phpeter.fr>
2014-09-14 16:13:20 +0200

    Begin 'about' page

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,3 +1,193 @@
-<div class="content">
+<div class="content" style="text-align:justify;font-size:18px;">
+	<h1 class="title">Qui sommes-nous ?</h1>
 	
+	<h3 class="title">Un peu d'histoire</h3>
+	<p>
+		<b>DreamVids</b> est une plateforme qui a vu le jour le 11 Décembre 2013.
+		À cette époque le scandale sur la nouvelle politique de YouTube vient d'éclater. Tout le monde,
+		même les plus grands YouTubers, s'indignent contre ces mesures. Je m'appelle Peter, j'ai alors 19
+		ans et je poste un floppée de tweet en disant que ce serait génial de lancer une toute nouvelle 
+		plateforme pile le jour de la mise en place de la nouvelle politique, le 1er Janvier 2014. Je tweet, 
+		je tweet, <a href="https://twitter.com/DarkWos1" target="_blank">@DarkWos1</a> et 
+		<a href="https://twitter.com/VincentBanana" target="_blank">@VincentBanana</a> me font savoir qu'ils 
+		sont prêts suivre le mouvement mais pour le moment ce ne sont que des tweets, jusqu'à ce que...
+		<blockquote class="twitter-tweet" lang="fr"><p><a href="https://twitter.com/p_cauty">@p_cauty</a> 
+		<a href="https://twitter.com/Experimentboy">@experimentboy</a> J&#39;te paye les serveurs, tu fait 
+		la plateforme, on partage ? ;)</p>&mdash; Jérémy Martin (@caaptusss) 
+		<a href="https://twitter.com/caaptusss/status/410833781021368320">11 Décembre 2013</a></blockquote>
+		<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		À partir de là le concept devient sérieux, on en parle sur Skype, on met en place un plan d'attaque, 
+		et c'est finalement avec <a href="https://twitter.com/oliviermis" target="_blank">@oliviermis</a> 
+		que le partenariat se fera.
+	</p>
+	
+	<h3 class="title">Et Maintenant ?</h3>
+	<p>
+		Aujourd'hui, DreamVids est un projet bénévole composé de 17 personnes passionnées.
+		Nous travaillons tous les jours pour faire de DreamVids une plateforme cool et reconnue !
+		Actuellement, vous naviguez sur la version 2 du site, qui fait suite à la version 1, qui était
+		une bêta, et qui a durée presque un an.
+	</p>
+	
+	<h3 class="title">L'équipe</h3>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="http://img11.hostingpics.net/pics/303239darkwoss.png" style="height:50px;width:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b>Dorian</b><br />
+				Darkwos, 17 ans, lycéen en filière ES, je suis passionné d'informatique,
+				de design, de jeux vidéo depuis toujours. Co-fondateur et web designer de DreamVids.
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="https://pbs.twimg.com/profile_images/489585405768245248/L6qJtra6.jpeg" style="height:50px;width:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b>Jeremy</b><br />
+				Jeremy, 15 ans, je gère sur DreamVids le community management (Twitter/Facebook),
+				je me passionne pour le développement web et les nouvelles technologies.
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="http://dreamvids.fr/uploads/lyonsuper/avatar.jpg" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b>Pierre alias Lyonsuper</b><br />
+				Hello moi c'est Pierre, je suis sous le pseudo Lyonsuper, je suis modérateur chez Dreamvids
+				je suis un grand fan des Sims comme on le remarque sur mon avatar et j'ai des compétences
+				en téléphonie mobiles si vous souhaiter discuter avec moi pas de soucis I'm Open !
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="http://snapcube.fr/avatar/avatar.jpeg" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b>Snapcube</b><br />
+				Moi c'est Alessandro, ou encore snapcube, un passionné d'informatique. J'aime plus
+				particulièrement la programmation. Modérateur chez Dreamvids, je vérifie vos faits et
+				gestes bandes de voyous ;)
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="https://pbs.twimg.com/profile_images/503533082465828864/UC55u98R_400x400.jpeg" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b>Louis A.</b><br />
+				Designer de DreamVids, étudiant en info, passionné de basket, de sport et de voyage.
+				Voilà ma vie en moins de 100 caractères. Suis-moi sur twitter aussi (@LowiSky_) !
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
+		<div style="margin-bottom:50px">
+			<div style="float:left">
+				<img src="" style="height:50px;border-radius:25px;" />
+			</div>
+			<p style="padding-left:70px">
+				<b></b><br />
+				
+			</p>
+		</div>
 </div>
