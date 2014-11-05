ca2811e2fc6f3720d7dd0c5b972f9a239718cf13
snapcube <taccolaaless@gmail.com>
2014-11-06 00:21:31 +0100

    Fixed empty playlist

---
+++ app/views/playlists/error_empty.php
@@ -0,0 +1,8 @@
+<section class="middle">
+	<h1 class="title">Playlist "<?php echo $playlist->name; ?>"</h1>
+	<div class="message error">
+			<div class="message-icn"><img src="/DEV_PRO/DreamVids/assets/img/message_error_icon.png" alt="Message d'erreur"></div>
+			<p>Cette playlist est vide</p>
+		</div>
+	
+</section>
