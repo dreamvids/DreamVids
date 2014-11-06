9c8fdd97b6419e58edf9e12e0b4a02d53a1de786
Snapcube <taccolaaless@gmail.com>
2014-11-06 21:18:20 +0100

    Update error_empty.php

--- app/views/playlists/error_empty.php
+++ app/views/playlists/error_empty.php
@@ -1,7 +1,7 @@
 <section class="middle">
 	<h1 class="title">Playlist "<?php echo $playlist->name; ?>"</h1>
 	<div class="message error">
-			<div class="message-icn"><img src="/DEV_PRO/DreamVids/assets/img/message_error_icon.png" alt="Message d'erreur"></div>
+			<div class="message-icn"><img src="<?php echo IMG . 'message_error_icon.png'; ?>" alt="Message d'erreur"></div>
 			<p>Cette playlist est vide</p>
 		</div>
 	
