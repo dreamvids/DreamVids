b8f02cd8b0c73fa2222a9fdcfd9396f36301a9a6
Maxime <mxcmaxime@users.noreply.github.com>
2014-12-15 22:49:28 +0300

    Ajout d'un encart pour les cookies :cookie:

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -17,7 +17,17 @@
 	</head>
 
 	<body>
+		
+		<!-- Encart pour les cookies mmmmh gateaux... -->
 
+		<div id="cookie-box">En navigant sur ce site vous <a onclick="closeCookie()">vous acceptez l'utilisation des cookies.</a></div>
+
+		<script>
+			function closeCookie(){document.getElementById("cookie-box").style.display="none";}
+		</script>
+
+		<!-- Encart pour les cookies mmmmh gateaux... -->
+		
 		<script>
 
 			var _currentpage_ = "<?php echo  isset($currentPage) ? $currentPage : 'default'; ?>";
