3a2bcb09c981fc106bd5a13a98e8efe6be7afe12
snapcube <taccolaaless@gmail.com>
2014-12-16 22:45:26 +0100

    Few adds

--- app/views/account/language.php
+++ app/views/account/language.php
@@ -1,7 +1,7 @@
 <div class="content">
 
 	<section class="profile">
-		<h1 class="title">Espace membre</h1>
+		<h1 class="title"><?php echo Translator::get("pages.account.title"); ?></h1>
 
 		<?php
 			include VIEW.'layouts/account_menu.php';
@@ -9,7 +9,7 @@
 
 		<form class="form" method="post" action="<?php echo WEBROOT.'account/language'; ?>">
 			<input type="hidden" name="_method" value="put" />
-			<label for="language"><?php echo Translator::get("pages.account.sub_menu.language.language_label")?></label>
+			<label for="language"><?php echo Translator::get("pages.account.sub_menu.language.language_label"); ?></label>
 			<select name="language" id="language">
 <?php foreach ($avaiable_languages as $value => $name) { 
 	$selected = Translator::getCurrentLanguageName() == $value ? "selected" : "";
