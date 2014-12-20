5c060aa591a3c639a2ecd1dfd76a4dc8abc183a0
PHPeter <phpeter@phpeter.fr>
2014-12-20 23:04:11 +0100

    Term Of Service

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,26 +1,26 @@
-<div class="content" style="text-align:justify;font-size:18px;">
+<div class="content" style="text-align:justify;">
 	<h1 class="title">Qui sommes-nous ?</h1>
-	
+
 	<h3 class="title">Un peu d'histoire</h3>
 	<p>
 		<b>DreamVids</b> est une plateforme qui a vu le jour le 11 Décembre 2013.
 		À cette époque le scandale sur la nouvelle politique de YouTube vient d'éclater. Tout le monde,
 		même les plus grands YouTubers, s'indignent contre ces mesures. Je m'appelle Peter, j'ai alors 19
-		ans et je poste un floppée de tweet en disant que ce serait génial de lancer une toute nouvelle 
-		plateforme pile le jour de la mise en place de la nouvelle politique, le 1er Janvier 2014. Je tweet, 
-		je tweet, <a href="https://twitter.com/DarkWos1" target="_blank">@DarkWos1</a> et 
-		<a href="https://twitter.com/VincentBanana" target="_blank">@VincentBanana</a> me font savoir qu'ils 
+		ans et je poste un floppée de tweet en disant que ce serait génial de lancer une toute nouvelle
+		plateforme pile le jour de la mise en place de la nouvelle politique, le 1er Janvier 2014. Je tweet,
+		je tweet, <a href="https://twitter.com/DarkWos1" target="_blank">@DarkWos1</a> et
+		<a href="https://twitter.com/VincentBanana" target="_blank">@VincentBanana</a> me font savoir qu'ils
 		sont prêts suivre le mouvement mais pour le moment ce ne sont que des tweets, jusqu'à ce que...
-		<blockquote class="twitter-tweet" lang="fr"><p><a href="https://twitter.com/p_cauty">@p_cauty</a> 
-		<a href="https://twitter.com/Experimentboy">@experimentboy</a> J&#39;te paye les serveurs, tu fait 
-		la plateforme, on partage ? ;)</p>&mdash; Jérémy Martin (@caaptusss) 
+		<blockquote class="twitter-tweet" lang="fr"><p><a href="https://twitter.com/p_cauty">@p_cauty</a>
+		<a href="https://twitter.com/Experimentboy">@experimentboy</a> J&#39;te paye les serveurs, tu fait
+		la plateforme, on partage ? ;)</p>&mdash; Jérémy Martin (@caaptusss)
 		<a href="https://twitter.com/caaptusss/status/410833781021368320">11 Décembre 2013</a></blockquote>
 		<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-		À partir de là le concept devient sérieux, on en parle sur Skype, on met en place un plan d'attaque, 
-		et c'est finalement avec <a href="https://twitter.com/oliviermis" target="_blank">@oliviermis</a> 
+		À partir de là le concept devient sérieux, on en parle sur Skype, on met en place un plan d'attaque,
+		et c'est finalement avec <a href="https://twitter.com/oliviermis" target="_blank">@oliviermis</a>
 		que le partenariat se fera.
 	</p>
-	
+
 	<h3 class="title">Et Maintenant ?</h3>
 	<p>
 		Aujourd'hui, DreamVids est un projet bénévole composé de 17 personnes passionnées.
@@ -28,7 +28,7 @@
 		Actuellement, vous naviguez sur la version 2 du site, qui fait suite à la version 1, qui était
 		une bêta, et qui a durée presque un an.
 	</p>
-	
+
 	<h3 class="title">L'équipe</h3>
 		<div style="margin-bottom:50px">
 			<div style="float:left">
@@ -67,8 +67,8 @@
 			</div>
 			<p style="padding-left:70px">
 				<b>Snapcube</b><br />
-				Moi c'est Alessandro, ou encore snapcube, un passionné d'informatique. 
-				J'aime plus particulièrement la programmation. J'étais un modérateur de Dreamvids 
+				Moi c'est Alessandro, ou encore snapcube, un passionné d'informatique.
+				J'aime plus particulièrement la programmation. J'étais un modérateur de Dreamvids
 				puis je suis devenu développeur juste avant la V2.
 			</p>
 		</div>
@@ -155,7 +155,7 @@
 				Je m’appelle Léo , mais je suis plus connu sous le pseudo de Gwomos et je suis Modéarteur.
 				J'ai 13 ans je suis passionné par l'informatique et l'audio visuel. J'adore bidouiller et
 				créer plein de choses plus ou moins utiles.
-				
+
 				N’hésitez pas à me dire si quelque chose ne va pas :)
 			</p>
 		</div>
