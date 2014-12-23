8b3504596f7ed43f96de223b4b92465e421e9f94
Quadrifoglio <quadrifoglio.clement@gmail.com>
2014-12-23 22:51:35 +1100

    Added publish date in video description

--- app/views/video/video.php
+++ app/views/video/video.php
@@ -51,7 +51,10 @@
 
 			<div class="inner-description">
 
-				<?php echo nl2br(preg_replace("#https?://[a-z0-9\./\+\,\%\#_\-\?\&\=\@\!\&]+#is", '<a href="$0" target="_blank">$0</a>', $description)).'<br /><br />Tags: ';
+				<?php
+				echo "Publiée le " . strftime("%e %B %Y", $video->timestamp) . "<br /><br />";
+				echo nl2br(preg_replace("#https?://[a-z0-9\./\+\,\%\#_\-\?\&\=\@\!\&]+#is", '<a href="$0" target="_blank">$0</a>', $description)).'<br /><br />Tags: ';
+
 				foreach ($tags as $tag) {
 					echo '<a href="'.WEBROOT.'search/%23'.$tag.'">#'.$tag.'</a> ';
 				}
