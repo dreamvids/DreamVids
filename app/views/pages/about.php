0822c4ed0b7181783879feab809a8216b9d6dcd1
mxcmaxime <join.mxcmaxime@outlook.fr>
2015-01-07 20:33:29 +0300

    Je me suis ajouté.

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -153,7 +153,7 @@
 			<p style="padding-left:70px">
 				<b>Maxime</b><br />
 				Graphiste, webdesigner et développeur front-end en autodidacte depuis 2010.<br />
-				J'ai intégré DreamVids avec la joie de découvrir une équipe soudée et un peu loufoque sur les bords.
+				J'ai intégré DreamVids avec la joie de découvrir une équipe soudée un peu loufoque sur les bords.
 			</p>
 		</div>
 		<div style="margin-bottom:50px">
