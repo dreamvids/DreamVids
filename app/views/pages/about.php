5a275cbd5ac50dfb68ce7bb23529dc1f03a7ae8d
mxcmaxime <join.mxcmaxime@outlook.fr>
2015-01-07 20:37:06 +0300

    je me suis ajouté

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -148,12 +148,12 @@
 		</div>
 		<div style="margin-bottom:50px">
 			<div style="float:left">
-				<img src="<?php echo IMG.'team/maxime.png'; ?>" style="height:50px;width:50px;border-radius:25px;" />
+				<img src="<?php echo IMG.'team/mxcmaxime.png'; ?>" style="height:50px;width:50px;border-radius:25px;" />
 			</div>
 			<p style="padding-left:70px">
 				<b>Maxime</b><br />
 				Graphiste, webdesigner et développeur front-end en autodidacte depuis 2010.<br />
-				J'ai intégré DreamVids avec la joie de découvrir une équipe soudée un peu loufoque sur les bords.
+				J'ai intégré DreamVids avec la joie de découvrir une équipe soudée et un peu loufoque sur les bords.
 			</p>
 		</div>
 		<div style="margin-bottom:50px">
