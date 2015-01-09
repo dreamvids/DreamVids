6b6ef85e2789d0e6c40b5e5bb639047167f48014
PHPeter <phpeter@phpeter.fr>
2015-01-09 23:26:25 +0100

    prepare the groundwork in order to dev V2 back-office

---
+++ app/controllers/admin/home_controller.php
@@ -0,0 +1,24 @@
+<?php
+require_once SYSTEM.'controller.php';
+require_once SYSTEM.'actions.php';
+require_once SYSTEM.'view_response.php';
+require_once SYSTEM.'redirect_response.php';
+
+class AdminHomeController extends Controller {
+	public function __construct() {
+		$this->denyAction(Action::GET);
+		$this->denyAction(Action::CREATE);
+		$this->denyAction(Action::UPDATE);
+		$this->denyAction(Action::DESTROY);
+	}
+	
+	public function index($request) {
+		echo 'It works.';
+		exit();
+	}
+	
+	public function get($id, $request){}
+	public function create($request){}
+	public function update($id, $request){}
+	public function destroy($id, $request){}
+}
