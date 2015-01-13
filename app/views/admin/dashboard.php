c5634cfab5b5becb8e1489439d37d11fabbe9375
PHPeter <phpeter@phpeter.fr>
2015-01-13 23:30:25 +0100

    Set SB-Admin-2 for admin template

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -1,12 +1,3 @@
-<div class="content">
-	<h1 class="title">Panel <?php echo $rankStr; ?> - Tableau de bord</h1>
-
-	<div class="user-info">
-		<h2><?php echo $user->username; ?> - <small><?php echo $rankStr ?></small></h2>
-
-		<p>Videos reportées: <?php echo $reportedVidsCount; ?></p>
-		<p>Commentaires reportés: <?php echo $reportedCommentsCount; ?></p>
-	</div>
-		
-	</div>
+<div class="row">
+	<h1>Dashboard</h1>
 </div>
