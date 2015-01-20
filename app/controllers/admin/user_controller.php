b5c1a2f99ba4c4884688da3d44a450dfba811c78
snapcube <taccolaaless@gmail.com>
2015-01-20 20:46:29 +0100

    [ADM] Added user edition

--- app/controllers/admin/user_controller.php
+++ app/controllers/admin/user_controller.php
@@ -3,12 +3,11 @@ require_once SYSTEM.'controller.php';
 require_once SYSTEM.'actions.php';
 require_once SYSTEM.'view_response.php';
 require_once SYSTEM.'redirect_response.php';
+require_once SYSTEM.'view_message.php';
 
 class AdminUserController extends Controller {
 	public function __construct() {
-		$this->denyAction(Action::GET);
 		$this->denyAction(Action::CREATE);
-		$this->denyAction(Action::UPDATE);
 		$this->denyAction(Action::DESTROY);
 	}
 	
@@ -17,9 +16,52 @@ class AdminUserController extends Controller {
 		$data['users'] = User::find('all');
 		return new ViewResponse('admin/user/index', $data);
 	}
-	
-	public function get($id, $request){}
-	public function create($request){}
-	public function update($id, $request){}
+	public function edit($id, $request) {
+		$data = [];
+		
+		if(User::exists(['id' => $id])){
+			$data['user'] = User::find($id);
+			$r = new ViewResponse("admin/user/edit", $data);
+		}else{
+			$r = new ViewResponse("admin/user/edit");
+			$r->addMessage(ViewMessage::error("L'utilisateur n'existe pas."));
+		}
+		return $r;
+	}
+	public function get($id, $request){
+		
+		return new RedirectResponse(WEBROOT . "admin/user/edit/$id");
+		
+	}
+	public function create($request){
+		echo 1;
+	}
+	public function update($id, $request) {
+		$data = $request->getParameters();
+		$deny_fields = ['id', 'rank'];
+		if (User::exists(['id' => $id])) {
+			$user = User::find(['id' => $id]);
+			foreach ($data as $k => $value) {
+				if(isset($user->$k) && !in_array($k, $deny_fields)){
+					switch ($k) {
+						case 'username':
+							$u_c = $user->getMainChannel();
+							$u_c->name = $value;
+							$u_c->save();
+						break;
+						case 'pass':
+							$value = !empty($value) ? password_hash($value, PASSWORD_BCRYPT) : $user->pass;
+						break;
+					}
+					
+					$user->$k = $value;
+				}
+				$user->save();
+			}
+			$r = new ViewResponse("admin/user/edit", ['user' => $user]);
+			
+		}
+		return $r;
+	}
 	public function destroy($id, $request){}
 }
--- app/views/admin/user/index.php
+++ app/views/admin/user/index.php
@@ -14,7 +14,7 @@
 				<?php foreach ($users as $user): ?>
 					<tr>
 						<td><a href="<?php echo WEBROOT.'channel/'.$user->getMainChannel()->id; ?>"><?php echo $user->username; ?></a></td>
-						<td><button class="btn-warning btn" onclick="alert('Pas encore implémenté')">Editer</button></td>
+						<td><a class="btn-warning btn" href="<?php echo WEBROOT . "admin/user/edit/$user->id" ?>">Editer</a></td>
 					</tr>
 				<?php endforeach ?>
 			</tbody>
