5d145342435f453b9d877bc0c1a15e0b71ea64f3
snapcube <taccolaaless@gmail.com>
2015-01-21 23:39:01 +0100

    [ADMIN] DataTable Theme

--- app/views/layouts/pages/admin/scripts.php
+++ app/views/layouts/pages/admin/scripts.php
@@ -1,10 +1,6 @@
 <script src="<?php echo JS.'admin_panel.js'; ?>"></script>
-<script>$(document).ready(function(){
-    $.extend( $.fn.dataTableExt.oStdClasses, {
-        "sFilterInput": "form-control",
-        "sLengthSelect": "form-control"
-    });
+<script>$(document).ready(function(){  
     $('.table-to-sort').DataTable();
-
 });
-	</script>
+</script>
+	<script type="text/javascript" src="<?php echo ASSETS . 'admin/js/DataTableTheme.js';?>"></script>
