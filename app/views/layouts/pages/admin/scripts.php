cc1347262527ed6782d739e43e051e64c754bcc9
snapcube <taccolaaless@gmail.com>
2015-01-21 23:44:52 +0100

    [ADMIN] number of item by page

--- app/views/layouts/pages/admin/scripts.php
+++ app/views/layouts/pages/admin/scripts.php
@@ -1,6 +1,8 @@
 <script src="<?php echo JS.'admin_panel.js'; ?>"></script>
 <script>$(document).ready(function(){  
-    $('.table-to-sort').DataTable();
+    $('.table-to-sort').DataTable({
+    	"iDisplayLength": 30
+        });
 });
 </script>
 	<script type="text/javascript" src="<?php echo ASSETS . 'admin/js/DataTableTheme.js';?>"></script>
