109460573905782e2f745f80711581949a5ef75a
snapcube <taccolaaless@gmail.com>
2015-01-23 22:25:06 +0100

    [ADMIN] Just 'cause it's swagg

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="<?php echo IMG.'favicon.png'; ?>" />
+    <link rel="icon" href="<?php echo IMG.'favicon_admin.png'; ?>" />
 
     <title>Administration - DreamVids</title>
 
