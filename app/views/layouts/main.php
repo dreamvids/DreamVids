5969091e2a12df21609ff20cedcde9ab94bbb948
PHPeter <phpeter@phpeter.fr>
2015-02-01 14:37:39 +0100

    Add analytics js code

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -210,11 +210,6 @@
 						
 					</div>
 
-					<!-- BETA UNIQUEMENT. A RETIRER AVANT LA PRODUCTION FINALE -->
-						<br>
-						<br>
-					<!-- / -->
-
 				</div>
 
 			</footer>
@@ -222,6 +217,15 @@
 		</div> <!-- #page -->
 
 		<?php isset($currentPage) ? include(VIEW.'layouts/pages/'.$currentPage.'/scripts.php') : include(VIEW.'layouts/pages/default/scripts.php'); ?>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-34423249-4', 'dreamvids.fr');
+			ga('send', 'pageview');
+		</script>
 
 	</body>
 
