395f5269c8b3047c639d3f7d00ebae1be45699ec
BrezhDev <contact@jouetromain.fr>
2015-02-10 14:19:21 +0100

    Fix CRLF injection/HTTP response splitting and prevent phising attack or malredirect link

--- system/redirect_response.php
+++ system/redirect_response.php
@@ -5,10 +5,18 @@ class RedirectResponse extends Response {
 	private $newURL = '';
 
 	public function __construct($newURL) {
-		if(filter_var($newURL, FILTER_VALIDATE_URL)) {	
-			$this->newURL = $newURL;
+		//Check if the passed url is a valid url
+		if(filter_var($newURL, FILTER_VALIDATE_URL)) {
+			//Prevent phising attack or malredirect link
+			$newURL_host = parse_url($newURL)["host"];
+			if ($_SERVER['HTTP_HOST'] == $newURL_host) {
+				$this->newURL = $newURL;
+			}else{
+				$this->newURL = '/';
+			}	
+		}else{
+			$this->newURL = '/';
 		}
-		$this->newURL = $newURL;
 	}
 
 	public function send() {
