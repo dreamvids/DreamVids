9b969f30043faa230b9b9c7bb03ddb374229967d
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2015-02-20 14:37:43 +0100

    New embed player

--- app/views/embed/video.php
+++ app/views/embed/video.php
@@ -9,7 +9,7 @@
 		<title>DreamVids</title>
 	</head>
 
-	<body class="embeded">
+	<body class="embeded" style="overflow: hidden;">
 
 		<script>
 
@@ -17,67 +17,48 @@
 
 		</script>
 
-		<div id="player">
-			<video x-webkit-airplay="allow" autobuffer <?php echo ($autoplay) ? 'autoplay' : ''; ?> preload="auto" poster="<?php echo $video->getThumbnail(); ?>">
-				<source id="srcMp4" type="video/mp4" src="">
-				<source id="srcWebm" type="video/webm" src="">
-			</video>
-			<div id="annotationsElement"></div>
-			<span id="repeat">
-				<span class="icon"></span>
-			</span>
-			<span id="qualitySelection" class="show"></span>
-			<span id="waitForPlay"></span>
-			<span id="bigPlay"></span>
-			<span id="bigPause"></span>
-			<div id="controls">
-				<span id="progress">
-					<span id="buffered"></span>
-					<span id="viewed"></span>
-					<span id="current"></span>
-				</span>
-				<span id="play-pause" class="play"></span>
-				<span id="time"></span>
-				<a href="http://dreamvids.fr/watch/<?php echo $video->id; ?>" target="_blank" id="DreamVidsIconEmbed"></a>
-				<span id="annotationsButton" style="display: none"></span>
-				<span id="qualityButton">SD</span>
-				<span id="volume">
-					<span id="barre"></span>
-					<span id="icon"></span>
-				</span>
-				<span id="separation"></span>
-				<span id="widescreen" class="widescreen"></span>
-				<span id="fullscreen" class="fullscreen"></span>
-			</div>
-		</div>
 
-		<script>var embeded = true;</script>
-		<script src="<?php echo JS.'player.js'; ?>"></script>
+    	<div id="player-div" class="watch-page-player" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; overflow: hidden;"></div>
+
+		<script src="<?php echo JS."dreamplayer.min.js"; ?>"></script>
+		<script src="<?php echo JS.'marmottajax.js'; ?>"></script>
 
 		<script>
 
-			setVideo([
+			new DreamPlayer({
+			
+			    cible: document.getElementById("player-div"),
+			    poster: "<?php echo $video->getThumbnail(); ?>",
 
-				{
+			    autoplay: <?php echo ($autoplay) ? 'true' : 'false'; ?>,
+			    embed: true,
+			
+			    sources: [
+			
+			        {
+			
+			            format: 360,
+			            text: "SD",
+			            mp4: "<?php echo $url; ?>_640x360p.mp4",
+			            webm: "<?php echo $url; ?>_640x360p.webm"
+			
+			        },
+			
+			        {
+			
+			            format: 720,
+			            text: "HD",
+			            mp4: "<?php echo $url; ?>_1280x720p.mp4",
+			            webm: "<?php echo $url; ?>_1280x720p.webm"
+			
+			        }
+			
+			    ]
+			
+			});
 
-					format: 360,
-					mp4: "<?php echo $url; ?>_640x360p.mp4",
-					webm: "<?php echo $url; ?>_640x360p.webm"
-
-				},
-
-				{
-
-					format: 720,
-					mp4: "<?php echo $url; ?>_1280x720p.mp4",
-					webm: "<?php echo $url; ?>_1280x720p.webm"
-
-				}
-
-			]);
-
-			setVolume(1);
 		</script>
+
 
 	</body>
 
