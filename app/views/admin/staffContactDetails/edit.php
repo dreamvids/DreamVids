2d84358c8c024619bfb85c4b8f49bffe288690fc
snapcube <taccolaaless@gmail.com>
2015-02-27 14:49:13 +0100

    Finish contact page in admin

---
+++ app/views/admin/staffContactDetails/edit.php
@@ -0,0 +1,39 @@
+<?php 
+	$tel_1 = '';
+	$tel_2 = '';
+	$email = $user->email;
+	$push_bullet_email = $user->email;
+if($user->getStaffDetails()){
+	$tel_1 = $user->getStaffDetails()->tel_1;
+	$tel_2 = $user->getStaffDetails()->tel_2;
+	$email = $user->getStaffDetails()->email;
+	$push_bullet_email = $user->getStaffDetails()->push_bullet_email;
+}
+?>
+
+<div class="row">
+	<h1>Vos coordonées <a href="<?php echo WEBROOT . 'admin/staffContactDetails'; ?>"><small>Retour à la liste</small></a></h1>
+
+	<div class="col-lg-4 col-lg-offset-4">
+		<form action="<?php echo WEBROOT . 'admin/staffContactDetails/' . $user->id; ?>" method="post">
+			<input type="hidden" name="_method" value="PUT">
+			
+			<label for="tel_1">N° de tel 1 :</label>
+			<input class="form-control" type="tel" name="tel_1" value="<?php echo $tel_1; ?>">
+			<br>
+			<label for="tel_2">N° de tel 2 :</label>
+			<input class="form-control"type="tel" name="tel_2" value="<?php echo $tel_2; ?>">
+			<br>
+			<label for="email">Adresse mail de contact :</label>
+			<input class="form-control"type="email" name="email" value="<?php echo $email; ?>">
+			<br>
+			<label for="push_bullet_email">Adresse mail PushBullet :</label>
+			<input class="form-control"type="email" name="push_bullet_email" value="<?php echo $push_bullet_email; ?>">
+			<br>
+			<input type="submit" class="btn btn-primary">
+			
+		</form>		
+	</div>
+</div>
+
+
---
+++ app/views/admin/staffContactDetails/index.php
@@ -0,0 +1,54 @@
+<div class="row">
+	<h1>Coordonnées du staff</h1>
+
+	<div class="col-lg-12">
+		<table class="table table-bordered table-hover table-striped">
+			<thead>
+				<tr>
+					<th>Nom d'utilisateur</th>
+					<th>Numéro(s) de tel</th>
+					<th>Adresse mail de contact</th>
+					<th>Adresse mail PushBullet</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<?php foreach ($infos as $k => $info): 
+					if(!is_object($info->details)){ ?>
+						<tr>
+							<td><?php echo $info->username; ?></td>
+							<td>[à remplir]</td>
+							<td>[à remplir]</td>
+							<td>[à remplir]</td>
+							<td>
+								<?php if($info->id == Session::get()->id) { ?>
+								<a class="btn-warning btn" href="<?php echo WEBROOT . 'admin/staffContactDetails/edit/' . $info->id; ?>">Editer</a>
+								<?php }else{ echo '--'; }?>
+							</td>
+						</tr>
+					<?php continue; }
+						$temp = $info->details;
+						$temp = Utils::secureActiveRecordModel($info->details);
+					?>
+
+					<tr>
+						<td><?php echo $info->username; ?></td>
+						<td><?php echo $info->details->tel_1 . "<br>" . $info->details->tel_2;  ?></td>
+						<td><?php echo $info->details->email ?></td>
+						<td><?php echo $info->details->push_bullet_email ?></td>
+						<td>
+							<?php if($info->id == Session::get()->id) { ?>
+							<a class="btn-warning btn" href="<?php echo WEBROOT . 'admin/staffContactDetails/edit/' . $info->id; ?>">Editer</a>
+							<?php }else{ echo '--'; }?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+
+		
+	</div>
+</div>
+
+
