2debe08c8d1a712563713449f1b927a0c330e1b1
snapcube <taccolaaless@gmail.com>
2015-05-05 13:05:49 +0200

    Few edits

--- migrate_subs.php
+++ migrate_subs.php
@@ -3,24 +3,25 @@ DEFINE('HOST', 'localhost'); 	//you know what these are
 DEFINE('DB', 'dreamvids_v2'); 
 DEFINE('USER', 'root');
 DEFINE('PASS', '');
-DEFINE('DOIT', false);
+DEFINE('DOIT', false); //For testing let it at false
 
-	echo "#This script will not modify other tables.\n\n";
+	echo "#This script will not modify other tables than `subscriptions`.\n\n";
 if(!DOIT){
 	echo "#Won't do anything : DOIT at false\n\n";
 }
 
 $create_table = 'DROP TABLE IF EXISTS `subscriptions`;
 CREATE TABLE IF NOT EXISTS `subscriptions` (
+  `id` INT(11) NOT NULL AUTOINCREMENT
   `user_id` int(11) NOT NULL,
   `channel_id` varchar(255) NOT NULL,
   `timestamp` bigint(20) NOT NULL,
-  PRIMARY KEY (`user_id`,`channel_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
 
 
-
 $pdo = new PDO('mysql:host='.HOST.';dbname='.DB, USER, PASS); //connect
+
 if(DOIT){
 	$pdo->query($create_table); //Init table
 }
@@ -29,13 +30,13 @@ $stmt = $pdo->query('SELECT id, subscriptions FROM `users`'); //On recupere tout
 
 $request = "INSERT IGNORE INTO `subscriptions` (`user_id`, `channel_id`, `timestamp`) VALUES"; //Début de requette
 
-while($current = $stmt->fetch(PDO::FETCH_ASSOC)){ //On recupert chaque user un par un
+while($current = $stmt->fetch(PDO::FETCH_ASSOC)){ //On recupere chaque user un par un
 
-	$subs = explode(';',trim($current['subscriptions'], ';')); //On recupert les subs
+	$subs = explode(';',trim($current['subscriptions'], ';')); //On recupere les subs
 	$user_id = $current['id']; //Le user_id
 	$done_for_this_user = [];
 	foreach($subs as $k => $sub){ //Pour chaque subscribe
-		if($sub =='' || in_array($sub, $done_for_this_user)){ //Si c'est vide "for some reason" on ignore' ou qu'il est dupliqué'
+		if($sub =='' || in_array($sub, $done_for_this_user)){ //Si c'est vide "for some reason" on ignore ou qu'il est dupliqué'
 			continue;
 		}
 		//sinon on ajoute une entrée à la requette du début;
