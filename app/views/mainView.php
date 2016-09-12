be18334658fe38dedb9c38b0c3aa82d754a6d0b1
Gynidark <gynidark@hotmail.com>
2016-09-12 22:10:11 +0200

    change html

--- app/views/mainView.php
+++ app/views/mainView.php
@@ -1,19 +1,21 @@
 <!doctype html>
 <html lang="fr">
 	<head>
-		<title><?php echo (isset($TITLE)) ? $TITLE.' - ' : ''; echo NAME; ?></title>
 		<meta charset="utf-8" />
-		<link type="text/css" rel="stylesheet" href="<?php echo CSS.'bootstrap.min.css'; ?>" media="screen" />
-		<link type="text/css" rel="stylesheet" href="<?php echo CSS.'style.css'; ?>" media="screen" />
 		<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
 		<meta name="format-detection" content="telephone=no" />
+
+		<title><?php echo (isset($TITLE)) ? $TITLE.' - ' : ''; echo NAME; ?></title>
+
+		<link type="text/css" rel="stylesheet" href="<?= CSS . 'bootstrap.min.css'; ?>" media="screen" />
+		<link type="text/css" rel="stylesheet" href="<?= CSS . 'style.css'; ?>" media="screen" />
 	</head>
 
 	<body>
 		<?php require_once @$appView; ?>
 		
-		<script type="text/javascript" src="<?php echo JS.'jquery.min.js'; ?>"></script>
-		<script type="text/javascript" src="<?php echo JS.'bootstrap.min.js'; ?>"></script>
-		<script type="text/javascript" src="<?php echo JS.'script.js'; ?>"></script>
+		<script type="text/javascript" src="<?= JS . 'jquery.min.js'; ?>"></script>
+		<script type="text/javascript" src="<?= JS . 'bootstrap.min.js'; ?>"></script>
+		<script type="text/javascript" src="<?= JS . 'script.js'; ?>"></script>
 	</body>
 </html>
