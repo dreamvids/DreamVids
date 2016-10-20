40f774aec6d9aee5bb28f8e418dae4057e6c8888
Gynidark <gynidark@gmail.com>
2016-10-20 03:21:10 +0200

    changing the readme!

---
+++ README.md
@@ -0,0 +1,3 @@
+# DreamVids
+
+![slim framework](https://img.shields.io/badge/framework-slim-orange.svg) ![version](https://img.shields.io/badge/version-3-brightgreen.svg) ![status](https://img.shields.io/badge/status-dev-red.svg) ![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)
---
+++ config/app.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'name' => 'DreamVids',
+    'description' => 'Plateforme de partage de vidéos libre et open-source',
+
+    'path' => [
+        'system' => '',
+        'app' => '',
+
+        'controllers' => '',
+        'views' => '',
+        'lang' => '',
+
+        'public' => [
+            'css' => '',
+            'js' => '',
+            'img' => ''
+        ],
+    ],
+];
